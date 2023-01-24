6c355cb884e5e6182c83843ba48b24df2e088d85
nihao35 <113234891+nihao35@users.noreply.github.com>
2023-01-24 01:17:52 +0000

    add test case (2023-01-24T01:17:52)

--- src/go/main2.go
+++ src/go/main2.go
@@ -1,2 +1,3 @@
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-01-15T17:43:58") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-01-17T23:30:16") }
+package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-01-24T01:17:52") }
