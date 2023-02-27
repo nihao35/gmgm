92bcc815023cebe029c52286aea56f6937eb37a9
nihao35 <113234891+nihao35@users.noreply.github.com>
2023-02-27 18:11:42 +0000

    fix bug in function (2023-02-27T18:11:42)

--- src/go/main1.go
+++ src/go/main1.go
@@ -2,3 +2,4 @@ package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-01-10T17:18:2
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-01-11T15:56:17") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-01-22T19:41:41") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-02-20T00:19:37") }
+package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-02-27T18:11:42") }
