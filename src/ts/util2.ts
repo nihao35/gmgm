2ee2d7447933298dd1937c2385d9cfd081297779
nihao35 <113234891+nihao35@users.noreply.github.com>
2023-04-15 22:14:24 +0000

    improve performance (2023-04-15T22:14:24)

--- src/ts/util2.ts
+++ src/ts/util2.ts
@@ -5,3 +5,4 @@ export const log = () => console.log('update 2023-04-03T07:10:26');
 export const log = () => console.log('update 2023-04-04T18:37:33');
 export const log = () => console.log('update 2023-04-14T01:29:12');
 export const log = () => console.log('update 2023-04-15T21:02:36');
+export const log = () => console.log('update 2023-04-15T22:14:24');
